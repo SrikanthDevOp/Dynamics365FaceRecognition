397ff68533295a2d6c17c177f39ffa534d154ae0
srikanth <srismiles438@gmail.com>
2018-04-16 18:21:08 +0530

    Added Face Identify Code.

--- Hsl.CognitiveServices.Demo/UserControl/FaceIdentify.xaml.cs
+++ Hsl.CognitiveServices.Demo/UserControl/FaceIdentify.xaml.cs
@@ -1,17 +1,14 @@
-﻿using System;
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Hsl.CognitiveServices.Demo.UserControl
 { 
@@ -20,6 +17,8 @@ namespace Hsl.CognitiveServices.Demo.UserControl
     /// </summary>
     public partial class FaceIdentify :ITabbed
     {
+        private string strSubscriptionKey = string.Empty;
+        private string strEndpoint = string.Empty;
         public FaceIdentify()
         {
             InitializeComponent();
@@ -30,10 +29,88 @@ namespace Hsl.CognitiveServices.Demo.UserControl
         /// </summary>
         public event Close CloseInitiated;
 
-        private void btnClose_Click(object sender, RoutedEventArgs e)
+    #region Events
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
         {
             CloseInitiated?.Invoke(this, new EventArgs());
         }
+        private void BtnNextKeyManagement_Click(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtSubscriptionKey.Text) || string.IsNullOrEmpty(txtEndpoint.Text))
+            {
+
+            }
+            else
+            {
+                this.strSubscriptionKey = txtSubscriptionKey.Text;
+                this.strEndpoint = txtEndpoint.Text;
+            }
+        }
+
+        private void BtnTrain_Click(object sender,RoutedEventArgs e)
+        {
+            try
+            {
+                List<Contact> lstContacts = new List<Contact>();
+                // Get the Contacts Images from CRM.
+                string strContactsImageQuery = @"<fetch mapping='logical'>
+                          <entity name='contact'>
+                            <attribute name='fullname'/>
+                            <attribute name='contactid'/>
+                            <attribute name='entityimage' />
+                          </entity>
+                        </fetch>";
+                EntityCollection entColContacts = CrmHelper._serviceProxy.RetrieveMultiple(new FetchExpression(strContactsImageQuery));
+                if(entColContacts==null || entColContacts.Entities.Count==0)
+                {
+                    return;
+                }
+                lstContacts = entColContacts.Entities
+                    .Where(ent => (ent.Attributes.Contains("entityimage") && ent.Attributes["entityimage"] != null))
+                    .Select(ent => new Contact
+                     {
+                         FullName = ent.Attributes["FullName"].ToString(),
+                         ImageBytes = ent.Attributes["entityimage"] as byte[],
+                         Id = ent.Attributes["contactid"].ToString()
+                     }).ToList();
+
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+
+        }
+        #endregion
+
+        private async void TrainContact(string personGroupId, byte[] contactImgBytes)
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            string strEndpoint = this.strEndpoint;
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", this.strSubscriptionKey);
+
+            var uri = $"{strEndpoint}/persongroups/{personGroupId}/train?" + queryString;
+
+            HttpResponseMessage response;
+
+            using (var content = new ByteArrayContent(contactImgBytes))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);
+            }
+    }
+
+    public  class Contact
+    {
+        public string FullName { get; set; }
+        public byte[] ImageBytes { get; set; }
+        public string Id { get; set; }
 
     }
+
 }
